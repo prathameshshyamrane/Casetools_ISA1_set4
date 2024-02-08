ffb0643c49a84f9a1f5d27c5caa7f29d8c8dbd16
prathameshshyamrane <psr261004@gmail.com>
2024-02-08 10:22:23 +0530

    commit

---
+++ Casetools_ISA1_set4/src/Casetools_ISA1_set4/circle.java
@@ -0,0 +1,17 @@
+package Casetools_ISA1_set4;
+
+public class circle {
+	private double radius;
+	private circle(double radius) { 
+		this.radius=radius;
+		
+	}
+	public double getRadius() {
+		return radius;
+	
+	}
+	public void setRadius(double radius) {
+		this.radius=radius;
+	}
+
+}
